de791d7611144552f9687579eaa7ce052660b348
ang3lcr <ang3lcortes@outlook.com>
2024-02-14 09:52:43 -0600

    fix

--- midsquareMethodPy/midSquare.py
+++ midsquareMethodPy/midSquare.py
@@ -1,17 +1,19 @@
-currentNumber = int(input("Enter a number: "))
-iterations = int(input("How much numbers you want to generate? "))
+seed = int(input("Enter a number (seed): "))
+iterations = int(input("How much numbers you want to generate?: "))
 
-randoms = []
-
-
+randomNumbers = []
 
 for i in range(iterations):
-    sqrNumber = currentNumber * currentNumber
-    cnLength = len(str())
-    num2string = str(sqrNumber)
-    middleNumber = int(num2string[len(num2string)//2 - 2: len(num2string)//2 + 2])
-    randoms.append(middleNumber)
-    currentNumber = middleNumber
+    sqrSeed = seed * seed
+    strSeed = str(sqrSeed)
+    while len(strSeed) < len(str(seed) * 2):
+        strSeed = '0' + strSeed
+    midIndex = len(strSeed) // 2
+    seed = int(strSeed[midIndex-2:midIndex+2])
+    randomNumbers.append(seed / 10000)
 
-print("[+] Generated numbers: ")
-print(randoms)
+
+print(randomNumbers)
+
+
+
